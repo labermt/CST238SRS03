3965ad777fc4c75f8c796fb272b52f55df1420b4
Ruby Felton <ruby.felton@oit.edu>
2018-07-31 22:00:31 -0700

    add custom view

--- app/src/main/java/com/example/rubyf/chaos/MainActivity.java
+++ app/src/main/java/com/example/rubyf/chaos/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.rubyf.chaos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.Random;
+import android.widget.Spinner;
 
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int num_verticies = 5;
-    public int iterations = 100;
-    public double distance = .5;
-    public int restriction = 0;
-    public int period = 0;
+    public int num_verticiesNum = 5;
+    public int iterationsNum = 100;
+    public double distanceNum = .5;
+    public int restrictionNum = 0;
+    public int periodNum = 0;
     Random rand = new Random();
     public int startPoint = rand.nextInt(5);
+    private Spinner distance;
+    private Spinner period;
+    private Spinner restriction;
+    private Spinner color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        distance = findViewById(R.id.distance);
+        period = findViewById(R.id.period);
+        restriction = findViewById(R.id.restriction);
+        color = findViewById(R.id.color);
     }
 }
