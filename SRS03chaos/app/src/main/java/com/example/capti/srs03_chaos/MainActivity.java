370fb0c52f9c2fd85656f1d446177d8a01daf1d3
BorkedFork <40620089+BorkedFork@users.noreply.github.com>
2018-08-11 22:56:17 -0700

    Pause for the night

--- SRS03chaos/app/src/main/java/com/example/capti/srs03_chaos/MainActivity.java
+++ SRS03chaos/app/src/main/java/com/example/capti/srs03_chaos/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.capti.srs03_chaos;
 
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+
+
+
 }
--- SRS03chaos/app/src/main/java/com/example/capti/srs03_chaos/PointMapper.java
+++ SRS03chaos/app/src/main/java/com/example/capti/srs03_chaos/PointMapper.java
@@ -4,30 +4,126 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Stack;
+
 public class PointMapper extends View
 {
     Paint paint_;
+    LinkedList<Point> InitialPoints;
+    LinkedList<Point> DrawnPoints;
+    int Height;
+    int Width;
+    public static Point Center;
+    public int Radius ;
+    public int RadiusFactor = 3;
+    double ShapeVertCount = 20;
+    Random rand;
+    int DotSize = 10;
+
 
     public PointMapper(Context context) {
         super(context);
         init(null, 0);
+
+        constructionSetup();
     }
 
     public PointMapper(Context context, AttributeSet attribs) {
         super(context, attribs);
         init(attribs, 0);
+
+        constructionSetup();
     }
 
     public PointMapper(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(attrs, defStyle);
+
+        constructionSetup();
     }
+
+    private void init(AttributeSet attribs, int defStyle)
+    {
+        paint_ = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint_.setStyle(Paint.Style.FILL);
+    }
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        constructionSetup();
+        CanvasPainter(canvas);
+    }
+
+    void constructionSetup()
+    {
+        rand = new Random((long)ShapeVertCount);
+        Center = new Point(Width/2,Height/2);
+        Radius = Height / RadiusFactor;
+        InitialPoints = new LinkedList<Point>();
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        Width = w;
+        Height = h;
+    }
+    protected void CanvasPainter(Canvas canvas)
+    {
+        ShapePoints();
+        for(Point p : InitialPoints)
+        {
+            canvas.drawCircle(p.x, p.y, DotSize, paint_);
+        }
+        VerticyChaser(canvas);
+    }
+
+    protected void ShapePoints()
+    {
+        for(double i = 1.0; i <= ShapeVertCount; i++)
+        {
+            double region = (i/ShapeVertCount);
+            double y = Math.sin((Math.PI * 2)* region);
+            double x = Math.cos((Math.PI * 2)* region);
+            InitialPoints.add(new Point( (int)(Center.x + (Radius * x)), (int)(Center.y + (Radius * y))));
+        }
+    }
+
+    private void VerticyChaser(Canvas canvas)
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            int Rando = rand.nextInt((int)ShapeVertCount);
+            Point Start = InitialPoints.get(Rando);
+            Point NextPoint = InitialPoints.get(rand.nextInt((int)ShapeVertCount));
+
+            DrawnPoints.add(NextPointPicker(Start,NextPoint));
+
+            canvas.drawCircle(DrawnPoints.getLast().x,DrawnPoints.getLast().y,DotSize,paint_);
+        }
+
+    }
+
+    private Point NextPointPicker(Point at, Point goingto)
+    {
+        int rando = rand.nextInt((int)ShapeVertCount);
+
+        Point NextPosition = new Point();
+
+        NextPosition.x = ((goingto.x - at.x) /2) + at.x;
+        NextPosition.y = ((goingto.y - at.y) /2) + at.y;
+
+        return NextPosition;
+    }
+}
+
 /*
     private void init(AttributeSet attrs, int defStyle) {
         // Load attributes
@@ -62,15 +158,3 @@ public class PointMapper extends View
         invalidateTextPaintAndMeasurements();
     }
 */
-
-    private void init(AttributeSet attribs, int defStyle)
-    {
-        paint_ = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint_.setStyle(Paint.Style.FILL);
-    }
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawCircle(500, 500, 200, paint_);
-    }
-
-}
